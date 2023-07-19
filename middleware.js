ee94263340a36332699175099f8e11eacbd96dc7
= <=>
2023-07-19 10:53:35 +0100

    annottations added to middleware

--- middleware.js
+++ middleware.js
@@ -1,8 +1,8 @@
-// Using Middlware
+// Using Middleware
 
 app.use((request, response, next) => {
-    //do something
-    next();
+    // Function
+    next();     // We do not define this - this is managed by the Express Module
 })
 
 // Every middleware function should either terminate with res.send() or call next(), otherwise the request will hang.
