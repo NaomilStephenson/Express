c1ad7ffcf99ee99a77b0d1578aea813c62a4608c
= <=>
2023-07-19 11:27:43 +0100

    parameters for navigation added

--- handlingRequests.js
+++ handlingRequests.js
@@ -15,3 +15,30 @@ app.delete('/home', (request, response)=>{                              // '/hom
 app.delete('/delete/:id', (request, response)=>{                        // '/;id' = Parameter ( Params)
     console.log(request.params.id);                                     // how to use the params
 });
+
+
+
+
+app.get("/", (request,response) => {                                    // Parameters for navigation
+    response.write("<h1> <centre> Welcome to the Employee Database </centre> </h1>");
+    response.write("<b> List of departments</b><br />");
+    response.write("<a href='/department/1'>HR department</a><br />");
+    response.write("<a href='/department/2'>IT department</a><br />");
+    response.write("<a href='/department/3'>Sales department</a><br />");
+});
+
+app.get("/department/:id",(request,response) => {
+    let dept = request.params.id;
+    let deptName;
+    if (dept == "1") {
+        deptName ="HR department";
+    };
+    if (dept == "2") {
+        deptName ="IT department";
+    };
+    if (dept == "3") {
+        deptName ="Sales department";
+    };
+    response.write(`<h1> <centre> ${deptName} </centre> </h1>`);
+    response.write("<a href='/'>Back</a><br />");
+});
