b63bf2b2a306b3703f65c8fc7c3d2c8e31035f9b
= <=>
2023-07-20 15:55:29 +0100

    working and second level multiple

--- tables.js
+++ tables.js
@@ -9,8 +9,18 @@ server.get("/",(request, response) => {
     response.end();
 });
 
-server.get("/timestable/:num",(request, response) => {
-    response.render("tables.ejs",{tn:request.params.num});
+server.get("/single/:num",(request, response) => {
+    response.render("single.ejs",{n:request.params.num});
+    response.end();
+});
+
+server.get("/second/:num",(request, response) => {
+    response.render("second.ejs",{n:request.params.num});
+    response.end();
+});
+
+server.get("/second/:num/:secnum",(request, response) => {
+    response.render("secondtables.ejs",{n:request.params.num,sn:request.params.secnum});
     response.end();
 });
 
